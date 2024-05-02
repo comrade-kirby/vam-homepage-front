36c4f3d7ba2e8adfcfd806bc3159619638b07856
max oppenheimer <maxoppenheimer1993@gmail.com>
2024-05-02 19:37:24 -0400

    add about placeholder

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { GraphControls, PageNav } from '$lib'
+</script>
+
+<PageNav let:onClick route="/works">
+  <p class="text-white">works content here</p>
+</PageNav>
+
+<GraphControls position="col-start-4 row-start-2 col-span-2" />

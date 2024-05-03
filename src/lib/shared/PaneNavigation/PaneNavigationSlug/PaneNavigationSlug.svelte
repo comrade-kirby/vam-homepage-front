f941cf47ddd4135d45791acd5b4bdce7034dcf51
max oppenheimer <maxoppenheimer1993@gmail.com>
2024-05-02 23:26:49 -0400

    hover icon text on minimized

--- src/lib/shared/PaneNavigation/PaneNavigationSlug/PaneNavigationSlug.svelte
+++ src/lib/shared/PaneNavigation/PaneNavigationSlug/PaneNavigationSlug.svelte
@@ -2,8 +2,8 @@
   export let onClick, minimizedLabelText, minimizedIconText
 </script>
 
-<button on:click={onClick} class="flex flex-auto items-center justify-start space-x-2">
-  <div class="uppercase bg-secondary/90 py-1 px-2 text-3xs font-bold text-dark/90 rounded-sm tracking-widest">
+<button on:click={onClick} class="group flex flex-auto items-center justify-start space-x-2">
+  <div class="uppercase bg-secondary/60 group-hover:bg-secondary/80 py-1 px-2 text-3xs font-bold text-dark/90 rounded-sm tracking-widest">
     {minimizedIconText}
   </div>
   <h1 class="text-xs md:text-sm text-primary/80 tracking-wider line-clamp-1">

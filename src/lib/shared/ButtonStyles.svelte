d7091adae90a9085edd6386758552fff7dea31d1
max oppenheimer <maxoppenheimer1993@gmail.com>
2024-05-02 23:22:29 -0400

    reduce ouline button border opacity

--- src/lib/shared/ButtonStyles.svelte
+++ src/lib/shared/ButtonStyles.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="group w-fit text-white/90 tracking-wide {center && 'w-full'} {xl && 'text-lg md:text-xl'} {xs && 'text-2xs md:text-xs'} 
-  { outline && 'px-4 py-2 border border-white/90 rounded-lg font-bold'
+  { outline && 'px-4 py-2 border border-white/60 rounded-lg font-bold'
   } { 
     layout && 'hover:border-secondary/90 hover:text-secondary/90'
   } {
